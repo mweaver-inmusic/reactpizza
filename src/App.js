90fd5f9e5e135d3929432183df7d500c9a637464
mweaver-inmusic <80358035+mweaver-inmusic@users.noreply.github.com>
2021-09-30 12:00:29 -0400

    testing ci=false

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Demo from './pages/demo.page';
 import Pizza from './pages/pizza.page';
 
 function App() {
   return (
     <>
       <Pizza />
+      {/* <Demo /> */}
     </>
   );
 }
